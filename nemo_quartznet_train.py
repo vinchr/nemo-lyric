2916c4f87d4ad47e104d3fd35657f634c1651205
Vinay Choudhary <vinchr@gmail.com>
2020-12-03 18:24:47 +0000

    Transcription test (non-functional)

--- nemo_quartznet_train.py
+++ nemo_quartznet_train.py
@@ -42,29 +42,53 @@ def train_asr(params):
     #trainer.test(test_manifest)
 
 
+def restore_asr(params, restore_path):
+    quartznet_model = nemo_asr.models.EncDecCTCModel(
+        cfg=DictConfig(params['model']))
+    quartznet_model.restore_from(restore_path)
+    return quartznet_model
+
+
 def validate_asr(asr_model, val_ds):
-    val_set = None
+    val_set = []
     with open(val_ds) as F:
-        val_set = json.load(val_ds)
-    val_files = [t["audio_filepath"] for t in val_set[0:4]]
-    transcription = asr_model.transcribe(val_files)
+        for line in F:
+            val = json.loads(line)
+            val_set.append(val)
+    val_files = [t["audio_filepath"] for t in val_set[0:1]]
+    print(val_files)
+    transcription = asr_model.transcribe(val_files, batch_size=32, logprobs=False)
+    print(transcription)
+    asr2 = pl.LightningModule.load_from_checkpoint("lightning_logs/version_2/checkpoints/epoch=1.ckpt")
+    transcription = asr2.transcribe(val_files, batch_size=32, logprobs=False)
+    print(transcription)
+    asr_pretrained = nemo_asr.models.EncDecCTCModel.from_pretrained(model_name="QuartzNet15x5Base-En")
+    transcription = asr_pretrained.transcribe(val_files, batch_size=32, logprobs=False)
     print(transcription)
 
 
 def main():
     parser = argparse.ArgumentParser(description="Example ASR Trainer")
     parser.add_argument("--model", required=False, default=QUARTZNET_PATH, type=str)
-    parser.add_argument("--train-ds", required=True, default=None, type=str)
+    parser.add_argument("--train-ds", required=False, default=None, type=str)
+    parser.add_argument("--restore", required=False, default=None, type=str)
     parser.add_argument("--val-ds", required=False, default=None, type=str)
     parser.add_argument("--save", required=False, default=None, type=str)
     args = parser.parse_args()
 
+    assert args.train_ds or args.restore
+    assert not (args.train_ds and args.restore)
+    assert not (args.restore and args.save)
+
     params = read_model_cfg(args.model, args.train_ds, args.val_ds)
-    asr_model = train_asr(params)
-    if args.save:
-        asr_model.save_to(args.save)
+    if args.restore:
+        asr_model = restore_asr(params, args.restore)
+    elif args.train_ds:
+        asr_model = train_asr(params)
+        if args.save:
+            asr_model.save_to(args.save)
     if args.val_ds:
-        validate_asr(asr_model)
+        validate_asr(asr_model, args.val_ds)
 
 
 if __name__ == '__main__':
